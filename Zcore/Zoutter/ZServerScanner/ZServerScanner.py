0fbbd21c6f0e62323c0c1a4f76ddc8c7fddee009
wangyouzi <1454691802@qq.com>
2016-03-04 16:09:38 +0800

    ZServerScanner secondly committed by youzi

--- Zcore/Zoutter/ZServerScanner/ZServerScanner.py
+++ Zcore/Zoutter/ZServerScanner/ZServerScanner.py
@@ -1,7 +1,7 @@
 #encoding:utf-8
 import sys
 sys.path.append("../..")
-import ZBaseCoreObject
+from ZBaseCoreObject import ZBaseCoreObject
 from time import sleep
 from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser,NmapParserException
@@ -14,17 +14,32 @@ from datetime import datetime
 
 
 
-class ZServerScanner(object):
+class ZServerScanner(ZBaseCoreObject):
     def __init_dbd__(self):
         try:
-            self.scan_report = bsddb.btopen(file = "result_server.db",flag = 'c')
+           
+            index = "result_server" + str(time.strftime('%Y%m%d%H%M%S')) + ".db"
+            self.scan_report = bsddb.btopen(file = index,flag = 'c')
             print "result_server is created successfully!"
+            self.filename.append(index)
+            self.filelist = bsddb.btopen(file = "filelist_re.db",flag = 'c')
+            self.filelist[str(time.strftime('%Y%m%d%H%M%S'))] = index
+          #  print self.filelist.items()
         except:
             print "Can't create BDB files!"
         
     def __init__(self,targets,options):
+        
         self.targets = targets
         self.options = options
+        """init_flag"""
+        self._flg_is_initing = False
+        self._flg_init_finished = False
+        """init start flag"""
+        if(self._flg_is_initing != True):
+            self._flg_is_initing =True
+        if(self._flg_init_finished != False):
+            self._flg_init_finished = False
         """scan flag"""
         self._flg_is_scanning = False
         self._flg_scan_finished = False
@@ -33,14 +48,28 @@ class ZServerScanner(object):
         self._flg_store_finished =False
         """BDB"""
         self.scan_report = None
+        self.filenlist =None
+        """存储文件名"""
+        self.filename = []        
         self.__init_dbd__()
         """重试次数&超时时间(s)"""
         self.retrycnt = 3
         self.timeout = 3600
         """处理端口状态"""
         self.port_states = ['open']
-        
+        """init finish flag"""
+        if(self._flg_is_initing!= False):
+            self._flg_is_initing = False
+        if(self._flg_init_finished != True):
+            self._flg_init_finished = True
+            
+            
     def do_scan(self):
+        """scan start flag"""
+        if(self._flg_is_scanning != True):
+            self._flg_is_scanning = True
+        if(self._flg_scan_finished != False):
+            self._flg_scan_finished = False
         """运行次数初始化"""
         trycnt = 0
         while True:
@@ -68,8 +97,12 @@ class ZServerScanner(object):
                         sleep(5)
                         runtime += 5
                 if nmap_proc.is_successful():
-                    self._flg_scan_finished = True
-                    self._flg_is_storing = False
+                    """scan finished flag"""
+                    if(self._flg_is_scanning != False):
+                        self._flg_is_scanning = False
+                    if(self._flg_scan_finished != True):
+                        self._flg_scan_finished = True
+                        
                     print '-' * 50
                     print nmap_proc.summary
                     return nmap_proc.stdout
@@ -83,6 +116,11 @@ class ZServerScanner(object):
                     
                         
     def parse_nmap_report(self,nmap_stdout):
+        """parse start flag"""
+        if(self._flg_is_storing != True):
+            self._flg_is_storing = True
+        if(self._flg_store_finished != False):
+            self._flg_store_finished = False        
         try:
             nmap_report = NmapParser.parse(nmap_stdout)
             self._flg_is_storing = True
@@ -93,14 +131,18 @@ class ZServerScanner(object):
                     tmp_host = host.address
             
                 for serv in host.services:
-                    if serv.state in self.port_states:
-                        self.scan_report[str(serv.port)+serv.protocol] = serv.service
-            self._flg_store_finished = True
-            self._flg_is_storing = False
+                   # if serv.state in self.port_states:
+                    self.scan_report[str(serv.port)+serv.protocol] = serv.service
+            """parse finished flag"""
+            if(self._flg_is_storing != False):
+                  self._flg_is_storing = False
+            if(self._flg_is_storing != True):
+                self._flg_is_storing = True                        
             print self.scan_report.items()
             return _flg_store_finished
         except Exception, e:
             return e
+        
 if __name__ == "__main__":
     task = ZServerScanner("127.0.0.1", "-sV")
     nmap_stdout = task.do_scan()
