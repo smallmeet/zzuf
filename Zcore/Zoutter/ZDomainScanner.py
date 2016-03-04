126df9100f6092dbca46cf9e83db047517785a47
mikaicebe <547878842@qq.com>
2016-03-04 15:04:11 +0800

    Update ZDomainScanner.py

--- Zcore/Zoutter/ZDomainScanner.py
+++ Zcore/Zoutter/ZDomainScanner.py
@@ -7,11 +7,17 @@ class ZDomainScanner(ZBaseCoreObject):
         self.domain = domain
 
     def getid(self):
-        myaddr = socket.getaddrinfo(self.domain,'http')[0][4][0]
-        return myaddr
+        try:
+            myaddr = socket.getaddrinfo(self.domain,'http')[0][4][0]
+            return myaddr
+        except:
+            return 350
 
     def getpz(self):
-        r = requests.get('http://s.tool.chinaz.com/same?s='+self.domain+'&page=')
-        responseHtml = r.content
-        match = re.findall(r'<div class="w30-0 overhid"><a href=\'(.*?)\'', responseHtml)
-        return match
+        try:
+            r = requests.get('http://s.tool.chinaz.com/same?s='+self.domain+'&page=')
+            responseHtml = r.content
+            match = re.findall(r'<div class="w30-0 overhid"><a href=\'(.*?)\'', responseHtml)
+            return match
+        except:
+            return 351
